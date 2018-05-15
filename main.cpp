6ace84abea1062cd1f433bfc77152b0f1f59ae47
Santiago Figueroa <37884952+SantiagoFigueroaMc@users.noreply.github.com>
2018-05-15 00:19:15 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <string>
-#include "io.h"
+#include "functions.h"
+using namespace std;
+char tablero [10][10];
+tablero[0][0]<<NULL;
 
-int main(){
-	mover_pieza("a1", "b3");
+int main(int argc, char**argv){
+	if (argc>1 && argv[1]=="-t"){
+		ifstream instruc;
+		instruc.open(argv[2]);}
+		//recibir codigo para instrucciones en archivo
+	else{
+		//recibir instrucciones por consola
+
 	return 0;
 }
