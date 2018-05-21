d812475499ed5c79882caa2ac267064dcff8e359
JavierMoragaPardo <39241655+JavierMoragaPardo@users.noreply.github.com>
2018-05-21 22:50:00 +0000

    Update chess.cpp

--- chess.cpp
+++ chess.cpp
@@ -1,7 +1,74 @@
+#include "chess.h"
 #include<string>
 
 using namespace std;
 
-void mover_pieza(string *pos_i, string *pos_f){
-	
-}
+void validezMovimiento(string pos_i,string pos_f){
+	string i, f;	
+	if (posi=="a1"){i=GetValue(a1)}
+	if (posi=="a2"){i=GetValue(a2)}
+	if (posi=="a3"){i=GetValue(a3)}
+	if (posi=="a4"){i=GetValue(a4)}
+	if (posi=="a5"){i=GetValue(a5)}
+	if (posi=="a6"){i=GetValue(a6)}
+	if (posi=="a7"){i=GetValue(a7)}
+	if (posi=="a8"){i=GetValue(a8)}
+	if (posi=="b1"){i=GetValue(b1)}
+	if (posi=="b2"){i=GetValue(b2)}
+	if (posi=="b3"){i=GetValue(b3)}
+	if (posi=="b4"){i=GetValue(b4)}
+	if (posi=="b5"){i=GetValue(b5)}
+	if (posi=="b6"){i=GetValue(b6)}
+	if (posi=="b7"){i=GetValue(b7)}
+	if (posi=="b8"){i=GetValue(b8)}
+	if (posi=="c1"){i=GetValue(c1)}
+	if (posi=="c2"){i=GetValue(c2)}
+	if (posi=="c3"){i=GetValue(c3)}
+	if (posi=="c4"){i=GetValue(c4)}
+	if (posi=="c5"){i=GetValue(c5)}
+	if (posi=="c6"){i=GetValue(c6)}
+	if (posi=="c7"){i=GetValue(c7)}
+	if (posi=="c8"){i=GetValue(c8)}
+	if (posi=="d1"){i=GetValue(d1)}
+	if (posi=="d2"){i=GetValue(d2)}
+	if (posi=="d3"){i=GetValue(d3)}
+	if (posi=="d4"){i=GetValue(d4)}
+	if (posi=="d5"){i=GetValue(d5)}
+	if (posi=="d6"){i=GetValue(d6)}
+	if (posi=="d7"){i=GetValue(d7)}
+	if (posi=="d8"){i=GetValue(d8)}
+	if (posi=="e1"){i=GetValue(e1)}
+	if (posi=="e2"){i=GetValue(e2)}
+	if (posi=="e3"){i=GetValue(e3)}
+	if (posi=="e4"){i=GetValue(e4)}
+	if (posi=="e5"){i=GetValue(e5)}
+	if (posi=="e6"){i=GetValue(e6)}
+	if (posi=="e7"){i=GetValue(e7)}
+	if (posi=="e8"){i=GetValue(e8)}
+	if (posi=="f1"){i=GetValue(f1)}
+	if (posi=="f2"){i=GetValue(f2)}
+	if (posi=="f3"){i=GetValue(f3)}
+	if (posi=="f4"){i=GetValue(f4)}
+	if (posi=="f5"){i=GetValue(f5)}
+	if (posi=="f6"){i=GetValue(f6)}
+	if (posi=="f7"){i=GetValue(f7)}
+	if (posi=="f8"){i=GetValue(f8)}
+	if (posi=="g1"){i=GetValue(g1)}
+	if (posi=="g2"){i=GetValue(g2)}
+	if (posi=="g3"){i=GetValue(g3)}
+	if (posi=="g4"){i=GetValue(g4)}
+	if (posi=="g5"){i=GetValue(g5)}
+	if (posi=="g6"){i=GetValue(g6)}
+	if (posi=="g7"){i=GetValue(g7)}
+	if (posi=="g8"){i=GetValue(g8)}
+	if (posi=="h1"){i=GetValue(h1)}
+	if (posi=="h2"){i=GetValue(h2)}
+	if (posi=="h3"){i=GetValue(h3)}
+	if (posi=="h4"){i=GetValue(h4)}
+	if (posi=="h5"){i=GetValue(h5)}
+	if (posi=="h6"){i=GetValue(h6)}
+	if (posi=="h7"){i=GetValue(h7)}
+	if (posi=="h8"){i=GetValue(h8)}
+	if (i=="  "){
+		cout << "posicion sin ninguna pieza, movimiento invalido" << endl;
+		return false;}
