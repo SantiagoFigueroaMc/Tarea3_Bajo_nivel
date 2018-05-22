ccbe680cdaf8b11299757c380a01e4027d57aeb7
JavierMoragaPardo <39241655+JavierMoragaPardo@users.noreply.github.com>
2018-05-22 03:46:50 +0000

    Add files via upload

--- chess.h
+++ chess.h
@@ -1,7 +1,13 @@
-#ifndef CHESS
-#define CHESS
+#ifndef _CHESS
+#define _CHESS
+
+#include <iostream>
+#include "node.h"
 #include <string>
 using namespace std;
 
-//Verifica validez del movimiento
-void validezMovimiento(string posi,string posf)
+//Verifica validez de la posicion inicial
+void validarPos_i (node nodei,int colorJugador)
+
+//Valida posicion final
+void validarPos_f (node nodei, int colorJugador)
